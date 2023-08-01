840f8f273bad1a5052b030c3189b4a52547c49f6
idsoid <125327978+idsoid@users.noreply.github.com>
2023-08-01 10:21:20 +0800

    webdeva2 menu animation

---
+++ webdeva2/script.js
@@ -0,0 +1,60 @@
+const hbutton=document.querySelector("#hbutton");
+const mbutton=document.querySelector("#mbutton");
+const tbutton=document.querySelector("#tbutton");
+var allpages=document.querySelectorAll(".page");
+const menuItemsList=document.querySelector("nav ul");
+const titlenav=document.querySelector("#titlenav")
+const hamBtn=document.querySelector("#hamIcon");
+
+//Pages
+console.log(allpages);
+function hideall() {
+    for(let onepage of allpages) {
+        onepage.style.display="none";
+    }
+}
+hideall();
+function show(pgno) {
+    hideall();
+    let onepage=document.querySelector("#page"+pgno);
+    onepage.style.display="block";
+}
+
+hbutton.addEventListener("click", function () {
+    show(1);
+});
+mbutton.addEventListener("click", function () {
+    show(2);
+});
+tbutton.addEventListener("click", function () {
+    show(3);
+});
+
+//Menu
+function toggleMenus() {
+    menuItemsList.classList.toggle("menuShow");
+    titlenav.classList.toggle("menuOpen");
+}
+hamBtn.addEventListener("click",toggleMenus);
+
+//Collapse/Expand
+var animateTimer;
+var content = hamBtn.nextElementSibling;
+function menuStart() {
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    }
+    else {
+        animateTimer = setInterval(MenuReveal, 0);
+    }
+}
+function MenuReveal() {
+    if (content.style.maxHeight) {
+        clearInterval(animateTimer);
+        animateTimer = null;
+    } 
+    else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
+hamBtn.addEventListener("click", menuStart);
