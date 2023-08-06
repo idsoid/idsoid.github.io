a8dc8cece67e3dc92e5ba0e8d07c0b0fe1149865
idsoid <125327978+idsoid@users.noreply.github.com>
2023-08-06 23:43:07 +0800

    final a2

--- webdeva2/script.js
+++ webdeva2/script.js
@@ -1,10 +1,13 @@
-const hbutton=document.querySelector("#hbutton");
-const mbutton=document.querySelector("#mbutton");
-const tbutton=document.querySelector("#tbutton");
-var allpages=document.querySelectorAll(".page");
-const menuItemsList=document.querySelector("nav ul");
-const titlenav=document.querySelector("#titlenav")
-const hamBtn=document.querySelector("#hamIcon");
+var hbutton = document.querySelector("#hbutton");
+var mbutton = document.querySelector("#mbutton");
+var tbutton = document.querySelector("#tbutton");
+var allpages = document.querySelectorAll(".page");
+var menuItemsList = document.querySelector("nav ul");
+var titlenav = document.querySelector("#titlenav");
+var hamBtn = document.querySelector("#hamIcon");
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
 //Pages
 console.log(allpages);
@@ -38,22 +41,130 @@ hamBtn.addEventListener("click",toggleMenus);
 
 //Collapse/Expand
 var animateTimer;
-var content = hamBtn.nextElementSibling;
-function menuStart() {
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    }
-    else {
-        animateTimer = setInterval(MenuReveal, 0);
-    }
+var collapsibles = document.getElementsByClassName("collapse");
+var i;
+for (i = 0; i < collapsibles.length; i++) {
+    collapsibles[i].addEventListener("click", function() {
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } 
+        else {
+            animateTimer = setInterval(Reveal(content), 0);
+        } 
+    });
 }
-function MenuReveal() {
-    if (content.style.maxHeight) {
+function Reveal(info) {
+    if (info.style.maxHeight) {
         clearInterval(animateTimer);
         animateTimer = null;
     } 
     else {
-        content.style.maxHeight = content.scrollHeight + "px";
+        info.style.maxHeight = info.scrollHeight + "px";
     }
 }
-hamBtn.addEventListener("click", menuStart);
+
+//Quiz
+var QuizQuestions = [
+	{
+		question: "Which ancient civilization made the soft pillows we all know and love?",
+		answers: {
+			A: 'Ancient Egypt',
+			B: 'Ancient Europe',
+			C: 'Ancient China'
+		},
+		correctAnswer: 'B'
+	},
+	{
+		question: "Which period were air mattresses made?",
+		answers: {
+			A: '1400s-1600s',
+			B: '1600s-1700s',
+			C: '1800s'
+		},
+		correctAnswer: 'C'
+	},
+    {
+		question: "Which material was the most popular for making pillows in Ancient China?",
+		answers: {
+			A: 'Wood',
+			B: 'Ceramic',
+			C: 'Bronze',
+            D: 'Jade',
+            E: 'Bamboo',
+            F: 'Porcelain'
+		},
+		correctAnswer: 'B'
+	},
+    {
+		question: "What is a con of Flannel?",
+		answers: {
+			A: 'Not suitable for hot climates',
+			B: 'Careful maintenance required to avoid fabric damage',
+			C: 'Process to make material involves harsh chemicals',
+            D: 'Can have coarse feel when new'
+		},
+		correctAnswer: 'A'
+	},
+    {
+		question: "What is a pro of Tencel?",
+		answers: {
+			A: 'Good option for people who have sensitive skin',
+			B: 'Wrinkle-resistant',
+			C: 'Widely available and easy to maintain',
+            D: 'Smooth, soft, and sleeps cool'
+		},
+		correctAnswer: 'D'
+	}
+];
+
+Quiz(QuizQuestions, quizContainer, resultsContainer, submitButton);
+
+function Quiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		var output = [];
+		var answers = [];
+
+		for(var i=0; i<questions.length; i++){
+			answers = [];
+			for(var letter in questions[i].answers){
+				//Radio Button
+				answers.push(
+					'<label>'+'<input type="radio" name="question'+i+'" value="'+letter+'">'+ letter + ': '+ questions[i].answers[letter]+ '</label>'
+				);
+			}
+
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		quizContainer.innerHTML = output.join('');
+	}
+	function showResults(questions, quizContainer, resultsContainer){
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		for(var i=0; i<questions.length; i++){
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			//Correct
+			if(userAnswer===questions[i].correctAnswer){
+				numCorrect++;
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			//Wrong
+			else{
+				answerContainers[i].style.color = 'red';
+			}
+		}
+		//Results
+		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+	}
+	showQuestions(questions, quizContainer);
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
